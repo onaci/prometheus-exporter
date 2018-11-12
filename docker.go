1f5fe62c4a50bbcece3c8bc96275a5ad4162be17
Dowideit, Sven (O&A, St. Lucia) <Sven.Dowideit@csiro.au>
2018-11-12 16:19:01 +1000

    add a small docker client that tells me the swarm nodes and their status
    Signed-off-by: Dowideit, Sven (O&A, St. Lucia) <Sven.Dowideit@csiro.au>

---
+++ docker.go
@@ -0,0 +1,90 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+
+	"github.com/docker/docker/api/types"
+	//"github.com/docker/docker/api/types/swarm"
+	"github.com/docker/docker/client"
+
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+)
+
+func init() {
+	// Since we are dealing with custom Collector implementations, it might
+	// be a good idea to try it out with a pedantic registry.
+	reg := prometheus.NewPedanticRegistry()
+
+	// Construct cluster managers. In real code, we would assign them to
+	// variables to then do something with them.
+	//NewDockerCollectorGatherer("db", reg)
+	NewDockerCollectorGatherer("ca", reg)
+
+	http.Handle("/metrics/docker", promhttp.HandlerFor(reg, promhttp.HandlerOpts{}))
+}
+
+// DockerCollectorGathererCollector implements the Collector interface.
+type DockerCollectorGathererCollector struct {
+}
+
+// Descriptors used by the DockerCollectorGathererCollector below.
+var (
+	dockerSwarmNodesDesc = prometheus.NewDesc(
+		"Docker_swarm_nodes",
+		"Number Docker Swarm nodes.",
+		[]string{
+			"host",
+			"role",
+			"availability",
+			"state",
+		}, nil,
+	)
+)
+
+// Describe is implemented with DescribeByCollect. That's possible because the
+// Collect method will always return the same two metrics with the same two
+// descriptors.
+func (cc DockerCollectorGathererCollector) Describe(ch chan<- *prometheus.Desc) {
+	prometheus.DescribeByCollect(cc, ch)
+}
+
+// Collect first triggers the ReallyExpensiveAssessmentOfTheSystemState. Then it
+// creates constant metrics for each host on the fly based on the returned data.
+//
+// Note that Collect could be called concurrently, so we depend on
+// ReallyExpensiveAssessmentOfTheSystemState to be concurrency-safe.
+func (cc DockerCollectorGathererCollector) Collect(ch chan<- prometheus.Metric) {
+	fmt.Println("get docker info")
+	cli, err := client.NewEnvClient()
+	if err != nil {
+		panic(err)
+	}
+	nodes, err := cli.NodeList(context.Background(), types.NodeListOptions{})
+	if err != nil {
+		panic(err)
+	}
+	for _, node := range nodes {
+		ch <- prometheus.MustNewConstMetric(
+			dockerSwarmNodesDesc,
+			prometheus.GaugeValue,
+			1,
+			node.Description.Hostname,
+			string(node.Spec.Role),
+			string(node.Spec.Availability),
+			string(node.Status.State),
+		)
+	}
+}
+
+// NewDockerCollectorGatherer first creates a Prometheus-ignorant DockerCollectorGatherer
+// instance. Then, it creates a DockerCollectorGathererCollector for the just created
+// DockerCollectorGatherer. Finally, it registers the DockerCollectorGathererCollector with a
+// wrapping Registerer that adds the zone as a label. In this way, the metrics
+// collected by different DockerCollectorGathererCollectors do not collide.
+func NewDockerCollectorGatherer(zone string, reg prometheus.Registerer) {
+	cc := DockerCollectorGathererCollector{}
+	prometheus.WrapRegistererWith(prometheus.Labels{"docker": "host"}, reg).MustRegister(cc)
+}
--- go.mod
+++ go.mod
@@ -1,11 +1,20 @@
 module main.go
 
 require (
+	docker.io/go-docker v1.0.0
 	github.com/beorn7/perks v0.0.0-20180321164747-3a771d992973 // indirect
+	github.com/docker/distribution v2.6.2+incompatible // indirect
+	github.com/docker/docker v1.13.1
+	github.com/docker/go-connections v0.4.0 // indirect
+	github.com/docker/go-units v0.3.3 // indirect
 	github.com/golang/protobuf v1.2.0 // indirect
 	github.com/matttproud/golang_protobuf_extensions v1.0.1 // indirect
+	github.com/opencontainers/go-digest v1.0.0-rc1 // indirect
+	github.com/opencontainers/image-spec v1.0.1 // indirect
+	github.com/pkg/errors v0.8.0 // indirect
 	github.com/prometheus/client_golang v0.9.1
 	github.com/prometheus/client_model v0.0.0-20180712105110-5c3871d89910 // indirect
 	github.com/prometheus/common v0.0.0-20181109100915-0b1957f9d949 // indirect
 	github.com/prometheus/procfs v0.0.0-20181005140218-185b4288413d // indirect
+	golang.org/x/net v0.0.0-20181108082009-03003ca0c849 // indirect
 )
